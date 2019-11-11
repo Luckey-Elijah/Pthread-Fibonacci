f60142ec575f79fab0eaead3308604e9b1f4e061
Elijah Luckey <luckeyelijah112@gmail.com>
2019-11-11 12:55:38 -0500

    Starting functional approach

--- elijah.c
+++ elijah.c
@@ -2,43 +2,25 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+int MAX_FIB_N = 29;
+int MIN_FIB_N = 1;
+
 // Functions used
 void *thread_runner(void *arg);
 int fib(int n);
-void main_prompt(int num);
+int check_and_return(int arg_count, const char *arg_value);
+void enter_prompt(int value);
+void exit_prompt(int value);
 
 // Global value for all threads to use
 int sum;
 
 int main(int argc, char const *argv[])
 {
-    // Check use part a
-    if (argc <= 1)
-    {
-        printf("USE: .\\elijah [number] \n");
-        printf("Improper use, exiting.\n");
-        exit(0);
-    }
+    int fib_num = check_and_return(argc, argv[1]);
+    enter_prompt(fib_num);
 
-    // Conversion to int with [atoi()]
-    int fib_num = atoi(argv[1]);
-
-    // Check use part b
-    if (fib_num < 1 || fib_num > 29)
-    {
-        printf("Please use an integer between 0 and 30.\n");
-        exit(0);
-    }
-
-    // prompt after checks
-    main_prompt(fib_num);
-
-    // Program starts here:
-
-    int *fib_sequence;
-    fib_sequence = (int *)malloc(fib_num * sizeof(int));
-
-    // thread identifier
+    // Threading starts up
     pthread_attr_t thread_attr;
     pthread_attr_init(&thread_attr);
     int i;
@@ -46,22 +28,50 @@ int main(int argc, char const *argv[])
     {
         pthread_t thread_id;
         // TODO: correct behaviour of thread
-        pthread_create(&thread_id, &thread_attr, thread_runner, &fib_sequence[i]);
+        pthread_create(&thread_id, &thread_attr, thread_runner, &fib_num);
         pthread_join(thread_id, NULL);
     }
-    printf("done:\t%d\n", sum);
+
+    exit_prompt(sum);
 
     return 0;
 }
 
-void main_prompt(int num)
+void exit_prompt(int value)
 {
-    printf("Welcome!\n");
-    printf("calculating fib_n = %d\n", num);
+    printf("+---------------------------------------+\n");
+    printf("| Goodbye.                              |\n");
+    printf("+---------------------------------------+\n");
+    return;
+}
 
-    printf("+-------------------------------+\n");
-    printf("| Calcuating %d with Threading\t|\n", num);
-    printf("+-------------------------------+\n");
+int check_and_return(int arg_count, const char *arg_value)
+{
+    if (arg_count < 2 || arg_count > 2)
+    {
+        printf("USE: ./elijah [number] \n");
+        printf("Improper use, exiting.\n");
+        exit(0);
+    }
+
+    // Conversion to int with [atoi()]
+    int int_value = atoi(arg_value);
+
+    // Check use part b
+    if (int_value < MIN_FIB_N || int_value > MAX_FIB_N)
+    {
+        printf("Please use an integer between [%d] and [%d].\n", MIN_FIB_N, MAX_FIB_N);
+        exit(0);
+    }
+
+    return int_value;
+}
+
+void enter_prompt(int value)
+{
+    printf("+---------------------------------------+\n");
+    printf("| Calcuating fib(%d) with Threading\t|\n", value);
+    printf("+---------------------------------------+\n");
     return;
 }
 
