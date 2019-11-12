e8205f38f222ec4b4d9974ae810c47998a69e0a3
Elijah Luckey <luckeyelijah112@gmail.com>
2019-11-12 09:56:45 -0500

    - Updated possible MAX_FIB_N - Added [free_int_array] function - updated global variable use - implmemtned thread and error handling for threads (simple) - EXIT_FAILURE and EXIT_SUCESS codes used instead of 1's and 0's

--- elijah.c
+++ elijah.c
@@ -3,50 +3,67 @@
 #include <pthread.h>
 
 // limits on the calculations
-int MAX_FIB_N = 29;
+int MAX_FIB_N = 47;
 int MIN_FIB_N = 1;
 
 // Functions used
 void nline(void);
 void *thread_runner(void *arg);
-int fib(int n);
 int check_and_return(int arg_count, const char *arg_value);
 int *create_int_array(int array_size);
+int free_int_array(int *array_pointer);
 void start_prompt(int value);
 void exit_prompt(int value);
 void print_int_array(int *array, int array_size);
 
 // Global value for all threads to use
-int sum;
+int fib_num;
+int *fib_seq;
 
 int main(int argc, char const *argv[])
 {
     // the size of the fibonacci sequence to the nth degre
-    int fib_num = check_and_return(argc, argv[1]);
+    fib_num = check_and_return(argc, argv[1]);
 
     // a simple user prompt
     start_prompt(fib_num);
-
-    // allocated a space for an array
-    int *fib_seq = create_int_array(fib_num);
+    fib_seq = create_int_array(fib_num);
 
     // populates first two elements of the array
     fib_seq[0] = 0;
     fib_seq[1] = 1;
 
-    int i;
-    for (i = 2; i < fib_num; i++)
+    // Start threading
+
+    pthread_t thread_x;
+
+    if (pthread_create(&thread_x, NULL, thread_runner, NULL))
     {
-        fib_seq[i] = fib_seq[i - 2] + fib_seq[i - 1];
+        printf("Could not create thread. Exiting.\n");
+        return EXIT_FAILURE;
+    }
+    if (pthread_join(thread_x, NULL))
+    {
+        printf("Could not join thread. Exiting.\n");
+        return EXIT_FAILURE;
     }
 
-    nline();
     print_int_array(fib_seq, fib_num);
-    nline();
+    exit_prompt(fib_num);
 
-    exit_prompt(sum);
+    return free_int_array(fib_seq);
+}
 
-    return 0;
+int free_int_array(int *array_pointer)
+{
+    free(array_pointer);
+
+    if (array_pointer != NULL)
+    {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
 
 // allocates space for an int array and returns a pointer
@@ -61,7 +78,7 @@ int *create_int_array(int array_size)
     {
         printf("Could not create array. Exiting.");
         nline();
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
     return array_pointer;
@@ -91,7 +108,7 @@ int check_and_return(int arg_count, const char *arg_value)
     {
         printf("USE: ./elijah [number] \n");
         printf("Improper use, exiting.\n");
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
     // Conversion to int with [atoi()]
@@ -101,7 +118,7 @@ int check_and_return(int arg_count, const char *arg_value)
     if (int_value < MIN_FIB_N || int_value > MAX_FIB_N)
     {
         printf("Please use an integer between [%d] and [%d].\n", MIN_FIB_N, MAX_FIB_N);
-        exit(0);
+        exit(EXIT_FAILURE);
     }
 
     return int_value;
@@ -119,22 +136,16 @@ void start_prompt(int value)
 // TODO: [thread_runner]
 void *thread_runner(void *arg)
 {
-    pthread_exit(0);
-    return;
-}
-
-// a reusable recursive use for calculatin a fibonacci sequence.
-int fib(int x)
-{
-    if (x < 2)
+    int element;
+    for (element = 2; element < fib_num; element++)
     {
-        return x;
+        fib_seq[element] = fib_seq[element - 2] + fib_seq[element - 1];
     }
-    return fib(x - 1) + fib(x - 2);
+    pthread_exit(EXIT_SUCCESS);
 }
 
 // print content of an integer array in a single line.
-// requires a point and the size of the array.
+// requires a pointer and the size of the array.
 void print_int_array(int *array, int array_size)
 {
     int i;
