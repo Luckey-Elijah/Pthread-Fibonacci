9c9576e6da9892e60462aabe582289ddcb745201
Elijah Luckey <luckeyelijah112@gmail.com>
2019-11-12 10:33:56 -0500

    - MAX_FIB_N is now 93 - added header comment - removed prompts - array data type now [unsigned long long int] to support higher fib n

--- elijah.c
+++ elijah.c
@@ -1,47 +1,67 @@
+/*
+
+Created: Elijah Luckey
+Course: Operating Systems Concepts COP4610
+Date: November 12, 2019
+
+Description: A program that uses the pthread API to generate an integer
+    array that is populated with the Fibonnaci sequence to the nth
+    degree. The largest nth value is managed by [MAX_FIB_N]
+    (which is currently [93]).
+
+Building:
+    make
+    or
+    gcc -g -Wall -pthread -o elijah elijah.c
+
+Use:
+    ./elijah [positive integer]
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
 // limits on the calculations
-int MAX_FIB_N = 47;
+int MAX_FIB_N = 93;
 int MIN_FIB_N = 1;
 
 // Functions used
 void nline(void);
 void *thread_runner(void *arg);
 int check_and_return(int arg_count, const char *arg_value);
-int *create_int_array(int array_size);
-int free_int_array(int *array_pointer);
-void start_prompt(int value);
-void exit_prompt(int value);
-void print_int_array(int *array, int array_size);
+unsigned long long int *create_int_array(int array_size);
+int free_int_array(unsigned long long int *array);
+void print_int_array(unsigned long long int *array, int array_size);
 
 // Global value for all threads to use
 int fib_num;
-int *fib_seq;
+unsigned long long int *fib_seq;
 
 int main(int argc, char const *argv[])
 {
     // the size of the fibonacci sequence to the nth degre
     fib_num = check_and_return(argc, argv[1]);
 
-    // a simple user prompt
-    start_prompt(fib_num);
     fib_seq = create_int_array(fib_num);
 
     // populates first two elements of the array
     fib_seq[0] = 0;
     fib_seq[1] = 1;
 
-    // Start threading
+    // Start threading section:
 
+    // Cread thread instance
     pthread_t thread_x;
 
+    // Call [pthread_create] and check validity
     if (pthread_create(&thread_x, NULL, thread_runner, NULL))
     {
         printf("Could not create thread. Exiting.\n");
         return EXIT_FAILURE;
     }
+    // Call [pthread_join] and check validity
     if (pthread_join(thread_x, NULL))
     {
         printf("Could not join thread. Exiting.\n");
@@ -49,16 +69,16 @@ int main(int argc, char const *argv[])
     }
 
     print_int_array(fib_seq, fib_num);
-    exit_prompt(fib_num);
 
     return free_int_array(fib_seq);
 }
 
-int free_int_array(int *array_pointer)
+// Uses [free()] to cleanup allocated memory of integer arrays.
+int free_int_array(unsigned long long int *array)
 {
-    free(array_pointer);
+    free(array);
 
-    if (array_pointer != NULL)
+    if (array != NULL)
     {
         return EXIT_FAILURE;
     }
@@ -66,37 +86,26 @@ int free_int_array(int *array_pointer)
     return EXIT_SUCCESS;
 }
 
-// allocates space for an int array and returns a pointer
-int *create_int_array(int array_size)
+// Allocates space for an int array and returns a pointer
+unsigned long long int *create_int_array(int array_size)
 {
-    int *array_pointer;
-
-    array_pointer = (int *)malloc(sizeof(int) * (array_size));
+    fib_seq = (unsigned long long int *)malloc(sizeof(unsigned long long int) * (array_size));
 
     // failed init
-    if (array_pointer == NULL)
+    if (fib_seq == NULL)
     {
         printf("Could not create array. Exiting.");
         nline();
         exit(EXIT_FAILURE);
     }
 
-    return array_pointer;
+    return fib_seq;
 }
 
 // just prints a newline
 void nline(void)
 {
     printf("\n");
-    return;
-}
-
-// Prompt to display on a succesful run.
-void exit_prompt(int value)
-{
-    printf("+---------------------------------------+\n");
-    printf("| Thank you. Now exiting.               |\n");
-    printf("+---------------------------------------+\n");
     return;
 }
 
@@ -124,15 +133,6 @@ int check_and_return(int arg_count, const char *arg_value)
     return int_value;
 }
 
-// Just a simple user prompt.
-void start_prompt(int value)
-{
-    printf("+---------------------------------------+\n");
-    printf("| Calcuating fib(%d) with threading.\t|\n", value);
-    printf("+---------------------------------------+\n");
-    return;
-}
-
 // Used for threading. A fibonacci sequence generator on the global
 // arry, fib_seq[].
 void *thread_runner(void *arg)
@@ -145,14 +145,14 @@ void *thread_runner(void *arg)
     pthread_exit(EXIT_SUCCESS);
 }
 
-// print content of an integer array in a single line.
-// requires a pointer and the size of the array.
-void print_int_array(int *array, int array_size)
+// Print content of an integer array in a single line.
+// Requires a pointer and the size of the array.
+void print_int_array(unsigned long long int *array, int array_size)
 {
-    int i;
+    unsigned long long int i;
     for (i = 0; i < array_size; i++)
     {
-        printf("%d ", array[i]);
+        printf("%lld ", array[i]);
     }
     nline();
     return;
